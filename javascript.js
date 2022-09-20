f6c07bd836c6097ac56895f8403a86003abaefb9
Kitteh3 <heyheymamakat@gmail.com>
2022-09-20 13:58:48 -0400

    Add functionality to grid slider and add grid divs

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
 //getting grid container to DOM
 const gridContainer = document.querySelector('.grid-container');
-//creating grid square div
-const gridSquare = document.createElement('div');
 
-//color picker from react-color
+
+/*color picker from react-color
 import React from 'react'
 import { SketchPicker } from 'react-color'
 
@@ -12,13 +11,26 @@ class Component extends React.Component {
   render() {
     return <SketchPicker />
   }
-};
+}; */
 
 //getting slider to DOM
 const slider = document.getElementById('myRange');
 const output = document.getElementById('demo');
 output.innerHTML = slider.value;
+
+//grid layout
+function makeGrid(rows, cols) {
+  gridContainer.style.setProperty('--grid-rows', rows);
+  gridContainer.style.setProperty('--grid-cols', cols);
+  for (let i = 0; i < (rows * cols); i++) {
+    let gridSquare = document.createElement('div');
+    gridContainer.appendChild(gridSquare).className = 'grid-square';
+  };
+}; 
+makeGrid(50, 50); //start with 50x50
 //update current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    output.innerHTML = this.value;
-}
+  output.innerHTML = this.value;
+  let value = Number(this.value);
+  makeGrid(value, value);
+};
